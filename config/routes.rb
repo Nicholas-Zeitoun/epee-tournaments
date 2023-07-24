c6d37fa01db322358c0ebdc517970d2993c141a6
NicholasZeitoun <nzeit8003@gmail.com>
2023-07-24 14:18:41 +1000

    [skip ci] test to skip

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     resources :participations, only: [:create, :delete]
   end
   resources :fencers
-  # root to: 'pages#home'
-  root to: 'tournaments#index'
+  root to: 'pages#home'
+  #root to: 'tournaments#index'
   # root to: 'fencers#index'
   get '/tournaments/:id/start', to: 'tournaments#start', as: 'start'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
